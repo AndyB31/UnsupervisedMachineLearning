4edd4f2703a628d56da49ecf639998b4911aaae0
lorenzodelmedico <lorenzodelmedico@gmail.com>
2023-06-06 10:03:13 +0200

    kmeansgenerate ok

--- kmeans/generate/main.py
+++ kmeans/generate/main.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
-    k = 11  # Number of clusters
-    no_of_iterations = 20  # Number of iterations for K-means
+    """**Generating Image**"""
+    k = 30  # Number of clusters
+    no_of_iterations = 50  # Number of iterations for K-means
     centroids = unsupervisedDeepLearning.kmeansgenerate(k, no_of_iterations) # Apply K-means clustering
+    print(centroids.shape)
+
     # Generate new digits from the cluster centroids
     generated_digits = centroids.reshape(k, 28, 28)
 
